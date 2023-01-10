88edf2d859fc379a03f98d388b014462fc534d53
«Nikita <blessmepadreee@gmail.com>
2023-01-10 22:18:34 +0300

    Коммит для второго урока финал

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let title = "Лессон 1";
 let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = "232";
-let rollback = "52";
-let fullPrice = " 50000";
+let screenPrice = 232;
+let rollback = 52;
+let fullPrice = 50000;
 let adaptive = true;
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice ,"рублей/ долларов/гривен/юани");
+console.log("Стоимость верстки экранов " + fullPrice ,"рублей/ долларов/гривен/юани");
+console.log(screens.toLowerCase());
+console.log(fullPrice * (rollback/100));
