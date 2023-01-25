a8e20fd6470c385c2ababa3404e2c6ea607a7068
Nikita Tepfer <blessmepadreee@gmail.com>
2023-01-25 16:13:07 +0300

    Вроде сработало коммит

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,10 +1,36 @@
-let num = 266219;
-let num1 = String(num);
-console.log(typeof num1);
-let split = num1.split("").map(Number);
-let num3 = split[0] + split[1] + split[2] + split[3] + split[4] + split[5];
-console.log(num3);
-let step = num3 ** 3;
-let splitstep = step.split("").map(Number);
-console.log(splitstep);
+let title;
+title = prompt("Как называется ваш проект?");
+let screens;
+screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice;
+screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive;
+adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1;
+service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1;
+servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2;
+service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2;
+servicePrice2 = +prompt("Сколько это будет стоить?");
+
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+console.log(fullPrice);
+let rollback = 52;
+let percent = fullPrice * (rollback / 100);
+let servicePercentPrice = Math.ceil(fullPrice - percent);
+console.log(servicePercentPrice);
+
+if (fullPrice > 30000) {
+    console.log("Даем скидку в 10%");
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+    console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 && fullPrice > 0) {
+    console.log("Скидка не предусмотрена");
+} else if (fullPrice < 0) {
+    console.log("Что-то пошло не так!");
+}
+
 
